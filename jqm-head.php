e03392e5f87d76d33de129bf425737f3899e39ad
Benjamin Ferrell <benjaminf333@gmail.com>
2014-06-18 19:59:55 -0400

    fixes some widgets
    Signed-off-by: Benjamin Ferrell <benjaminf333@gmail.com>

--- jqm-head.php
+++ jqm-head.php
@@ -10,14 +10,13 @@
 function generateJQMHeader()
 {
     $html =
-        '<head>'
-        . '<meta name="viewport" content="width=device-width, initial-scale=1">'
-        . '<script src="js/jquery-1.11.0.min.js"></script>'
-        . '<script src="jqm/jquery.mobile-1.4.2.min.js"></script>'
-        . '<link href="jqm/jquery.mobile.structure-1.4.2.min.css" rel="stylesheet">'
-        . '<link href="jqm/jquery.mobile.theme-1.4.2.min.css" rel="stylesheet">'
-        . '<title>GG Main</title>'
-        . '</head>';
+        '<head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="js/jquery-1.9.1.min.js"></script>
+        <script src="jqm/jquery.mobile-1.4.2.min.js"></script>
+        <link href="jqm/jquery.mobile-1.4.2.min.css" rel="stylesheet">
+        <title>GG Main</title>
+        </head>';
     echo $html;
 }
 
