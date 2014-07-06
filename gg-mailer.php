d91f81f9e48b202ba476b8919ed44accb264874c
srqhivemind <srqhivemind@gmail.com>
2014-07-06 00:29:33 -0400

    bug fixes for gg-mailer

--- gg-mailer.php
+++ gg-mailer.php
@@ -3,13 +3,52 @@
 # It only has obe major function:
 $message = "This is an automated report from GMAN.\r\n";
 
-# Include the absolute path to the functions
-set_include_path("D:\wamp\www");
-include("functions.php");
-
 $page_libs = "user";
-
 require_once("function-loader.php");
+
+### This is mostly a shell script thing
+// Enable Mail functionality
+require_once "Mail.php";  
+
+  
+/* SMTP server name, port, user/passwd */  
+
+
+function gg_mail($to,$subject,$message){
+    $smtpinfo["host"] = "ssl://smtp.gmail.com";  
+    $smtpinfo["port"] = "465";  
+    $smtpinfo["auth"] = true;  
+    $smtpinfo["username"] = "geekspacegwinnett@gmail.com";  
+    $smtpinfo["password"] = "g33ksp4c3Mail3r!!!@@2";
+
+   $from    = "geekspacegwinnett@gmail.com";  
+   
+    $sub = "GMAN: ".$subject;  
+    $body    = $message;
+    $footer=<<<EOF
+
+-----
+Geekspace Gwinnett Automailer
+Geekspace Gwinnett, Inc.
+This message was sent to ".$to."
+Please do not reply to this message.
+
+EOF;
+    $body=$body.$footer;    
+    $headers = array ('From' => $from,'To' => $to,'Subject' => $subject);  
+    $smtp = &Mail::factory('smtp', $smtpinfo );  
+    $mail = $smtp->send($to, $headers, $body);  
+  
+    if (PEAR::isError($mail)) {  
+    database_update($databases['gman'],"insert into maillog set content='".$mail->getMessage()."'");
+    return FALSE;
+ } else {  
+  return TRUE;
+ }  
+    
+}
+
+
 
 $expiring_members = get_expiring_members(7);
 $expired_members = get_delinquent_members(30);
@@ -74,7 +113,7 @@ foreach ($emails['result'] as $staff)
 }
 
 #print_r($send_to);
-$subject = "GMAN Weekly Member Report;"
+$subject = "GMAN Weekly Member Report";
 
 foreach ($send_to as $to)
 {
